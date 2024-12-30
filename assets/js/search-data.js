aa67228381b4b264e1c754946a634776868627e9
sr-dash <51014191+sr-dash@users.noreply.github.com>
2024-12-30 22:30:29 +0000

    Deploying to gh-pages from @ sr-dash/sr-dash.github.io@72247ad6eb5d18faaa3d433d17d803f34c9fa6f4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -130,9 +130,9 @@ ninja.data = [{
           description: "Inferred active region properties retrived from Time-Distance Helioseismology method will be added into SFT evolution.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
+            },},{id: "projects-dkist-data-analysis",
+          title: 'DKIST Data analysis',
+          description: "Working with Cryo-NIRSP data",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-project-5",
